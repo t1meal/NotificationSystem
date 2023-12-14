a72b4c51cc403dbe91199a3b2b5394cc5d316982
Алексей <t1meal@yandex.ru>
2023-12-14 15:58:59 +0300

    Initial commit

---
+++ src/main/java/ru/fcahp/system/NotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.fcahp.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationSystemApplication.class, args);
+	}
+
+}
